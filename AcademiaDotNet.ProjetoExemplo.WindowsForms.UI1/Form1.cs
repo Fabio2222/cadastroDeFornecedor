41ee23288afa26b289e93e2edc302c7dae260c01
Fabio2222 <fabiomoreira73@outlook.com>
2021-12-14 12:54:15 -0300

    first commit

---
+++ AcademiaDotNet.ProjetoExemplo.WindowsForms.UI1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcademiaDotNet.ProjetoExemplo.WindowsForms.UI1
+{
+    public partial class FFormularioInicial : Form
+    {
+        public FFormularioInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+            var fornecedor = new Fornecedor();
+
+            
+                                   
+            List<Fornecedor> listafornecedores = new List<Fornecedor>();
+            listafornecedores.Add(RetornarFornecedor(fornecedor));
+
+            dgvFornecedores.DataSource = listafornecedores;
+        }
+
+        private Fornecedor RetornarFornecedor(Fornecedor fornecedor)
+        {
+            if (txtNomeFornecedor.Text != string.Empty)
+            {
+                fornecedor.NomeDoFornecedor = txtNomeFornecedor.Text;
+            }
+            else
+            {
+                MessageBox.Show("Você não digitou o nome do fornecedor!");
+                
+            }
+
+            if (txtEndereco.Text != string.Empty)
+            {
+                fornecedor.Endereco = txtEndereco.Text;
+            }
+            else
+            {
+                MessageBox.Show("Você não digitou o endereço!");
+                
+            }
+
+            if (txtNomeContato.Text != string.Empty)
+            {
+                fornecedor.NomeContato = txtNomeContato.Text;
+            }
+            else
+            {
+                MessageBox.Show("Você não digitou o nome de contato!");
+                
+            }
+
+            if (txtTelefone.Text != string.Empty)
+            {
+                fornecedor.Telefone = txtTelefone.Text;
+            }
+            else
+            {
+                MessageBox.Show("Você não digitou o telefone!");
+                
+            }
+
+            return fornecedor;
+        }
+    }
+}
---
+++ AcademiaDotNet.ProjetoExemplo.WindowsForms.UI1/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaDotNet.ProjetoExemplo.WindowsForms.UI1
+{
+    public class Fornecedor
+    {
+
+       public string NomeDoFornecedor { get; set; } 
+       public string Endereco { get; set; }
+       public string NomeContato { get; set; }
+       public string Telefone { get; set; }
+
+    }
+}
